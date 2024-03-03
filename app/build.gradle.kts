a97f6343f638191ede94dcd4514fe2211b5d92df
Shivangi <shivangit1111@gmail.com>
2024-03-03 23:48:18 +0530

    build: add coroutines dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
     ksp("androidx.room:room-compiler:2.5.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
 
 }
